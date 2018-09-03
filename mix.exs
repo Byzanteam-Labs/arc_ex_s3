81cb0b4721f96cbe8d544c4e3b9a980d0d585f2b
CptBreeza <cbasicok@gmail.com>
2018-09-04 02:06:44 +0800

    Update deps

--- mix.exs
+++ mix.exs
@@ -25,6 +25,7 @@ defmodule ArcExS3.MixProject do
   defp deps do
     [
       {:arc, "~> 0.10.0"},
+      {:ex_arc, "~> 0.0.1", github: "GreenNerd-Labs/ex_arc", branch: "develop"},
       {:ex_aws, "~> 2.0"},
       {:ex_aws_s3, "~> 2.0"},
       {:httpoison, "~> 1.2.0", override: true}
